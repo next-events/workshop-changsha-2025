e61e67a20c111bf8bb65b6546263c818f3cd022e
Xiaohao Liu <xiaohao.liu@hotmail.com>
2025-04-18 14:11:23 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/workshop-changsha-2025',
   distDir: 'docs', // Optional: change the default build directory
   reactStrictMode: true,
 }
